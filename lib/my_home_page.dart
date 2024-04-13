3d331a37bf705734f82165286c48c09a7a92464c
alhasann351 <alhasann352@gmail.com>
2024-04-13 20:34:46 +0600

    appbar-homepage-created

---
+++ lib/my_home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: const Text(
+          'GetX Height Width',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text('GetX Height Width')
+      ),
+    );
+  }
+}
